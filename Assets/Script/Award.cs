2024c8d4d7d4a5b74e3203e41d4325287bc9eea4
ye1088 <18631616220@163.com>
2017-11-05 08:15:41 +0800

    备份

---
+++ Assets/Script/Award.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Award : MonoBehaviour {
+
+    public int type = 0; // 0 Gun 1 explode
+
+    public float speed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (-4.6f > this.transform.position.y)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int hp = 1;
+
+    public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (-5.22f > this.transform.position.y)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
